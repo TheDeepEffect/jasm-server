aa7ebb8699278ca69491a8daf4b6c9d2324b9011
Deep Manek <deepmanek25@gmail.com>
2021-09-26 20:54:27 +0530

    Likes subscription updated

--- src/resolvers/Subscriptions/Like.ts
+++ src/resolvers/Subscriptions/Like.ts
@@ -1,11 +1,11 @@
 import { Prisma } from "@prisma/client";
 import { nonNull, stringArg, subscriptionField } from "nexus";
 
-export const latestLike = subscriptionField("latestLike", {
+export const latestLikes = subscriptionField("latestLikes", {
   type: "Like",
   args: { id: nonNull(stringArg()) },
   subscribe(root, args, ctx) {
-    return ctx.pubsub.asyncIterator("latestLike");
+    return ctx.pubsub.asyncIterator("latestLikes");
   },
   resolve(root: Prisma.LikeGetPayload<{}>, args, ctx) {
     const { id } = args;
