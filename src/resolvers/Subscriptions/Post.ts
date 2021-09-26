9a56796124ae67aaf93bb674336e941c2da19a75
Deep Manek <deepmanek25@gmail.com>
2021-09-26 17:40:41 +0530

    Post subscription updated

--- src/resolvers/Subscriptions/Post.ts
+++ src/resolvers/Subscriptions/Post.ts
@@ -2,11 +2,25 @@ import { Prisma } from "@prisma/client";
 import { subscriptionField } from "nexus";
 
 export const latestPost = subscriptionField("latestPost", {
-    type: 'Post',
-    subscribe(root, args, ctx) {
-        return ctx.pubsub.asyncIterator("latestPost");
-    },
-    resolve(payload) {
-        return payload as Prisma.PostGetPayload<{}>
-    }
-})
+  type: "Post",
+  subscribe(root, args, ctx) {
+    return ctx.pubsub.asyncIterator("latestPost");
+  },
+  async resolve(root: Prisma.PostGetPayload<{}>, args, ctx) {
+    const currentPost = await ctx.prisma.post.findFirst({
+      where: {
+        AND: [
+          { id: root.id },
+          { NOT: { authorId: ctx.userId } },
+          {
+            OR: [
+              { author: { followers: { some: { followBy: ctx.userId } } } },
+              { isPrivate: false },
+            ],
+          },
+        ],
+      },
+    });
+    return currentPost as Prisma.PostGetPayload<{}>;
+  },
+});
